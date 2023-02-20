3e70acaf8b4bf135cf69c21b46bbbfb9466dfd58
Joel Jütte <j.juette.inf20@stud.bbbaden.ch>
2023-02-20 17:24:58 +0100

    Initial GUI

---
+++ Data/Word.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuetteJoel_LB151.Data
+{
+    public class Word
+    {
+        public string word { get; set; }
+        public string category { get; set; }
+
+        public Word(string word)
+        {
+            this.word = word;
+        }
+    }
+}
